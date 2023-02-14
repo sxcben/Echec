b476e7a5825c490ecaed76730b566f157bc1681a
Hakim506 <124039932+Hakim506@users.noreply.github.com>
2023-02-14 10:20:32 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "tictactoe2.h"
+#include "echecs3.h"
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <array>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+// tictactoe2 humain vs ordi
+int main()
+{
+    Grille grille;
+    Jouer(grille);
+    return 0;
+}
