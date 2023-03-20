57c6811f96a253c98eea69a1e77db54c0e5d6914
Hakim506 <124039932+Hakim506@users.noreply.github.com>
2023-03-20 18:37:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,18 +8,13 @@
 #include <ctime>
 #include <cstdlib>
 
-
-
 using namespace std;
 
-// tictactoe2 humain vs ordi
 int main()
 {
-
 //    Grille grille;
 //    Jouer(grille);
-    Jouer_echec();
-    //cout << "\033[1;37;46m" << "Hello World" << "\033[0m" << endl;
-
+    JouerUnJoueur();
+    JouerDeuxJoueurs();
     return 0;
 }
